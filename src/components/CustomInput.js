4246f130d889130c55bbcae3e79ce8cca76cdff5
julianxlozano <julianxlozano@gmail.com>
2021-10-01 17:50:58 -0500

    added custom label

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -5,28 +5,15 @@ const CustomInput = (props) => {
     const [customValue,setCustomValue] = useState("");
     const [customFieldName,setCustomFieldName] = useState("Custom Field")
 
-
-    // const createCustomField = (e) =>{
-    //  e.preventDefault()
-    //     const newField = {
-    //         [customFieldName]: customValue
-    //     }
-
-    //     let customIndex = props.customFields.findIndex(f=> f.id === props.newField.id)
-    //     const newCustomFieldsArray = [...props.customFields]
-    //     newCustomFieldsArray[customIndex] = newField
-    //     props.setCustomFields(newCustomFieldsArray)
-    // }
-
     const createCustomFieldValue = (value) =>{
-        //debugger
+ 
         setCustomValue(value)
         console.log(value)
         const newField = {
             id: props.newField.id,
             [customFieldName]: customValue
         }
-        //debugger
+   
         console.log(props.newField.id)
         let customIndex = props.customFields.findIndex(f=> f.id === props.newField.id)
         console.log(customIndex)
@@ -54,9 +41,9 @@ const CustomInput = (props) => {
 
         return (
             <div>
+            <label >Custom</label>
             <input onChange={e=>createCustomFieldName(e.target.value)} type="text" className="form-control"  placeholder="name of custom field" ></input>
             <input onChange={e=>createCustomFieldValue(e.target.value)} type="text" className="form-control"  placeholder="value of custom field"></input>
-            {/*<button onClick={e=>createCustomField(e)} type="button" className="btn btn-success email-btn">Save Custom</button>*/}
             </div>
         );
  
