8646cf55ba88ecb92c8487aba304f730104b51cc
julianxlozano <julianxlozano@gmail.com>
2021-10-02 21:27:46 -0500

    added logic to correctly format form field labels

--- src/components/EditContactForm.js
+++ src/components/EditContactForm.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import EditCustomInput from './EditCustomInput';
+
 
 const EditContactForm = (props) => {
 
@@ -18,8 +18,6 @@ const EditContactForm = (props) => {
     const handleSumbit = (e) =>{
         
         e.preventDefault()
-        //console.log(document.getElementsByClassName('edit-fields'))
-
         const newContact = {}
         const allFields = e.target.children
   
@@ -29,16 +27,6 @@ const EditContactForm = (props) => {
                 newContact[allFields[i].name] = allFields[i].value
             }
         }
-
-
-
-        // const newContact = {
-        //     id: props.contact.id,
-        //     firstName: firstName || props.contact.firstName,
-        //     lastName: lastName || props.contact.lastName,
-        //     phoneNumber: phoneNumber || props.contact.phoneNumber
-        // }
-
 
         let contactIndex = props.allContacts.findIndex(contact=> contact.id === props.contact.id)
         const newContactsArray = [...props.allContacts]
@@ -52,45 +40,18 @@ const EditContactForm = (props) => {
         let keyNum = 0
         for (const deet in props.contact){
             if (deet === 'id') continue;
+
+            let newString = deet.replace(/([A-Z])/g, ' $1');
+            let formattedDeet = newString.charAt(0).toUpperCase() + newString.slice(1);
+
             deets.push(
-            <label>{deet}</label>,
+            <label>{formattedDeet}</label>,
             <input key={keyNum} className="edit-fields form-control" name={deet} id="dynamic-input"defaultValue={props.contact[deet]}></input>
             )
             keyNum += 1
         }
         return deets
     } 
-
-    //  useEffect(() => {
-       
-    //     for (const field in props.contact){
-    //         if (field === 'id' || field === 'firstName' || field === 'lastName' || field === 'phoneNumber') continue;
-    //         setCustomFields([...customFields,{id: uuid(),[field]: props.contact[field]}])
-    //     }
-    //     // return (
-    //     //     customFields.map(customField=><EditCustomInput key={customField.id} newField={customField} customFields={customFields} setCustomFields={setCustomFields}/>)
-    //     // )
-    // },[])
-
-
-    //     return (
-    //         <form className="edit-form" onSubmit={handleSumbit}> 
-    //             <div className="form-group">
-    //                 <label for="FormControlInput1">First Name</label>
-    //                 <input onChange={e=>setFirstName(e.target.value)} type="text" className="form-control" id="FormControlInput2" defaultValue={props.contact.firstName}></input>
-    //                 <label for="FormControlInput1">Last Name</label>
-    //                 <input onChange={e=>setLastName(e.target.value)} type="text" className="form-control" id="FormControlInput3" defaultValue={props.contact.lastName}></input>
-    //                 <label for="FormControlInput1">Phone Number</label>
-    //                 <input onChange={e=>setPhoneNumber(e.target.value)} type="text" className="form-control" id="FormControlInput4" defaultValue={props.contact.phoneNumber}></input>
-    //                 {/*Object.keys(props.contact).length > 3 ? getCustomFields() : null*/}
-    //                 {Object.keys(props.contact).length > 3 ? customFields.map(customField=><EditCustomInput key={customField.id} newField={customField} customFields={customFields} setCustomFields={setCustomFields} />) : null}
-    //             </div>
-    //             <div className="btn-group">
-    //             <input type="submit" className="btn btn-success email-btn" id="FormControlInput5" ></input>
-    //             </div>
-    //     </form>
-    //     );
-
 
         return(
             <form className="edit-form" onSubmit={handleSumbit}>
