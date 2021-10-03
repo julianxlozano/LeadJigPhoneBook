30103a0f2ae30e3d9463c4b289db258671f5d9a8
julianxlozano <julianxlozano@gmail.com>
2021-10-02 20:58:10 -0500

    trying new dynamic form strategy

--- src/components/EditContactForm.js
+++ src/components/EditContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import EditCustomInput from './EditCustomInput';
 
 const EditContactForm = (props) => {
@@ -8,7 +8,12 @@ const EditContactForm = (props) => {
     const [phoneNumber,setPhoneNumber] = useState(null)
     const [customFields,setCustomFields] = useState([])
 
- 
+    const uuid = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+          });
+      }
 
     const handleSumbit = (e) =>{
         e.preventDefault()
@@ -27,33 +32,57 @@ const EditContactForm = (props) => {
         props.setEditing(false)
     }
 
-    const getCustomFields = () => {
-        const customFieldsToEdit = []
-        for (const field in props.contact){
-            if (field === 'id' || field === 'firstName' || field === 'lastName' || field === 'phoneNumber') continue;
-            customFieldsToEdit.push({[field]: props.contact[field]})
+    const getDetails = () => {
+        let deets = []
+        let keyNum = 0
+        for (const deet in props.contact){
+            if (deet === 'id') continue;
+            deets.push(
+            <label for="FormControlInput1">{deet}</label>,
+            <input key={keyNum} className="form-control" defaultValue={props.contact[deet]}></input>
+            )
+            keyNum += 1
         }
-
-        return (
-            customFieldsToEdit.map(customField=><EditCustomInput key={customField.id} customField={customField}/>)
-        )
+        return deets
     } 
-        return (
-            <form className="edit-form" onSubmit={handleSumbit}> 
-                <div className="form-group">
-                    <label for="FormControlInput1">First Name</label>
-                    <input onChange={e=>setFirstName(e.target.value)} type="text" className="form-control" id="FormControlInput2" defaultValue={props.contact.firstName}></input>
-                    <label for="FormControlInput1">Last Name</label>
-                    <input onChange={e=>setLastName(e.target.value)} type="text" className="form-control" id="FormControlInput3" defaultValue={props.contact.lastName}></input>
-                    <label for="FormControlInput1">Phone Number</label>
-                    <input onChange={e=>setPhoneNumber(e.target.value)} type="text" className="form-control" id="FormControlInput4" defaultValue={props.contact.phoneNumber}></input>
-                    {Object.keys(props.contact).length > 3 ? getCustomFields() : null}
-                </div>
-                <div className="btn-group">
-                <input type="submit" className="btn btn-success email-btn" id="FormControlInput5" ></input>
-                </div>
-        </form>
-        );
+
+    //  useEffect(() => {
+       
+    //     for (const field in props.contact){
+    //         if (field === 'id' || field === 'firstName' || field === 'lastName' || field === 'phoneNumber') continue;
+    //         setCustomFields([...customFields,{id: uuid(),[field]: props.contact[field]}])
+    //     }
+    //     // return (
+    //     //     customFields.map(customField=><EditCustomInput key={customField.id} newField={customField} customFields={customFields} setCustomFields={setCustomFields}/>)
+    //     // )
+    // },[])
+
+
+    //     return (
+    //         <form className="edit-form" onSubmit={handleSumbit}> 
+    //             <div className="form-group">
+    //                 <label for="FormControlInput1">First Name</label>
+    //                 <input onChange={e=>setFirstName(e.target.value)} type="text" className="form-control" id="FormControlInput2" defaultValue={props.contact.firstName}></input>
+    //                 <label for="FormControlInput1">Last Name</label>
+    //                 <input onChange={e=>setLastName(e.target.value)} type="text" className="form-control" id="FormControlInput3" defaultValue={props.contact.lastName}></input>
+    //                 <label for="FormControlInput1">Phone Number</label>
+    //                 <input onChange={e=>setPhoneNumber(e.target.value)} type="text" className="form-control" id="FormControlInput4" defaultValue={props.contact.phoneNumber}></input>
+    //                 {/*Object.keys(props.contact).length > 3 ? getCustomFields() : null*/}
+    //                 {Object.keys(props.contact).length > 3 ? customFields.map(customField=><EditCustomInput key={customField.id} newField={customField} customFields={customFields} setCustomFields={setCustomFields} />) : null}
+    //             </div>
+    //             <div className="btn-group">
+    //             <input type="submit" className="btn btn-success email-btn" id="FormControlInput5" ></input>
+    //             </div>
+    //     </form>
+    //     );
+
+
+        return(
+            <form className="edit-form" onSubmit={handleSumbit}>
+                {getDetails()}
+            </form>            
+
+        )
  
 }
 
--- src/components/EditCustomInput.js
+++ src/components/EditCustomInput.js
@@ -3,41 +3,29 @@ import React, { useState } from 'react';
 const EditCustomInput = (props) => {
 
     const [customValue,setCustomValue] = useState("");
-    const [customFieldName,setCustomFieldName] = useState("Custom Field")
-
 
     const createCustomFieldValue = (value) =>{
+ 
         setCustomValue(value)
-        console.log(value)
-        const newField = {
-            [customFieldName]: customValue
-        }
 
+        const newField = {
+            id: props.newField.id,
+            [Object.keys(props.newField)[0]]: customValue
+        }
+   
+        console.log(props.newField.id)
         let customIndex = props.customFields.findIndex(f=> f.id === props.newField.id)
+        console.log(customIndex)
         const newCustomFieldsArray = [...props.customFields]
         newCustomFieldsArray[customIndex] = newField
         props.setCustomFields(newCustomFieldsArray)
     }
 
-    const createCustomFieldName = (name) =>{
-        setCustomFieldName(name)
-        console.log(name)
-        const newField = {
-            [customFieldName]: customValue
-        }
-
-        let customIndex = props.customFields.findIndex(f=> f.id === props.newField.id)
-        const newCustomFieldsArray = [...props.customFields]
-        newCustomFieldsArray[customIndex] = newField
-        props.setCustomFields(newCustomFieldsArray)
-    }
 
         return (
             <div>
-                
-            <label for="FormControlInput1">{Object.keys(props.customField)[0]}</label>
-            <input onChange={e=>createCustomFieldValue(e.target.value)} type="text" className="form-control"  placeholder="value of custom field" defaultValue={props.customField[Object.keys(props.customField)[0]]}></input>
-  
+            <label for="FormControlInput1">{Object.keys(props.newField)[1]}</label>
+            <input onChange={e=>createCustomFieldValue(e.target.value)} type="text" className="form-control"  placeholder="value of custom field" defaultValue={props.newField[Object.keys(props.newField)[1]]}></input>
             </div>
         );
  
